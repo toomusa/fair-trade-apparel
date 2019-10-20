433297b2f4aed4f3f6b12697bac00ca732074a4e
Denzel Hartshorn <denzelhartshorn@gmail.com>
2019-10-20 10:14:53 -0700

    explorer worked on

--- src/pages/Explorer.js
+++ src/pages/Explorer.js
@@ -1,13 +1,24 @@
 // highlight brands, trends, consumer ratings
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
+// import Carousel from "../partials/Carousel.js";
 
 export default class Explorer extends Component {
   render() {
     return (
       <div>
+        {/* <Carousel /> */}
         <h1>Explore Page</h1>
+        <h2>Top 5 Performer Lists</h2>
+        <h3>1. Materials</h3>
+        <h3>2. Processs</h3>
+        <h3>3. Environmental Impact</h3>
+        <h3>4. Storage and Shipping</h3>
+        <h3>4. Environmentally Conscious</h3>
+        <h3>5. Worker Conditions</h3>
+        <h3>6. Social Responsibility</h3>
+        {/* top underperformers */}
       </div>
-    )
+    );
   }
 }
--- src/partials/Navbar.js
+++ src/partials/Navbar.js
@@ -1,30 +1,46 @@
-import React from "react"
+import React from "react";
 
 export default function Navbar() {
-  return(
+  return (
     <div className="navbar">
       <div id="navtext">
         <a href="/" className="link">
-          {window.location.pathname === "/"
-            ? <span><strong>Home</strong></span>
-            : <span>Home</span>}
+          {window.location.pathname === "/" ? (
+            <span>
+              <strong>Home</strong>
+            </span>
+          ) : (
+            <span>Home</span>
+          )}
         </a>
         <a href="/certification" className="link">
-          {window.location.pathname === "/certification"
-            ? <span><strong>Certification</strong></span>
-            : <span>Certification</span>}
+          {window.location.pathname === "/certification" ? (
+            <span>
+              <strong>Certification</strong>
+            </span>
+          ) : (
+            <span>Certification</span>
+          )}
         </a>
         <a href="/explore" className="link">
-          {window.location.pathname === "/explore"
-            ? <span><strong>Explore</strong></span>
-            : <span>Explore</span>}
+          {window.location.pathname === "/explore" ? (
+            <span>
+              <strong>Explore</strong>
+            </span>
+          ) : (
+            <span>Explore</span>
+          )}
         </a>
         <a href="/products" className="link">
-          {window.location.pathname === "/products"
-            ? <span><strong>Products</strong></span>
-            : <span>Products</span>}
+          {window.location.pathname === "/products" ? (
+            <span>
+              <strong>Products</strong>
+            </span>
+          ) : (
+            <span>Products</span>
+          )}
         </a>
       </div>
     </div>
-  )
+  );
 }
