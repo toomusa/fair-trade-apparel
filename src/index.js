03c34863c619e600f7f48d44ae688f14023fa75d
toomusa <3895499+toomusa@users.noreply.github.com>
2019-10-19 18:13:13 -0700

    fix manifest

--- src/index.js
+++ src/index.js
@@ -9,10 +9,4 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-<<<<<<< HEAD
 // serviceWorker.unregister();
-=======
-serviceWorker.unregister();
-
-// add an comment here gs1 us
->>>>>>> 3d687aa377224d869d304b76103ae74b90ed178c
